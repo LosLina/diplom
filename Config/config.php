ff2d6d16b3f07bce21edcf3a7f0ee21964d6041d
LosLina <101467225+LosLina@users.noreply.github.com>
2024-04-14 12:33:20 +0300

    Add files via upload

---
+++ Config/config.php
@@ -0,0 +1,27 @@
+<?php
+$Config =
+    [
+        'Database' =>
+            [
+                'Server' => 'Localhost',
+                'Username' => 'root',
+                'Password' => 'root',
+                'Database' => 'cms3'
+            ],
+        'NewsCount' => 4,
+        'CCount' => 6,
+        'Count' => 10,
+        'Forum' => 15,
+        'offset' => 4,
+        'BlogCount' => 2,
+        'CatalogCount' => 25,
+        'InfoCount' => 15,
+        'SerCount' => 20,
+        'BasketCount' => 30,
+        'PriceCount' => 25,
+        'WayCount' => 25,
+        'RespCount' => 15,
+        'NoteCount' => 20,
+        'OprosCount' => 10
+
+    ];
